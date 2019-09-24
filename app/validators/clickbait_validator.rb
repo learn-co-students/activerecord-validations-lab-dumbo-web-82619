28825ed0e85a4027e21109505975bfb80695cd2a
BennyLouie <bennylouie1412@gmail.com>
2019-09-24 15:27:08 -0400

    Done.

--- app/validators/clickbait_validator.rb
+++
@@ -1,7 +0,0 @@
-class ClickbaitValidator < ActiveModel::Validator
-    def validator(record)
-        if record.title.include?("Won't Believe", "Secret", "Top[number]", "Guess")
-            record.errors[:title] << "This is clickbait!"
-        end
-    end
-end
